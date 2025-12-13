9cb58f85093378ef49cf8d8654b804ed04fefbaa
Alena <nanoptushka@proton.me>
2025-12-14 02:07:50 +0300

    Add files via upload

--- RayRender/main.c
+++ RayRender/main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <math.h>
 #include <raylib.h>
@@ -5,25 +6,13 @@
 /* SCREEN SIZE PARAMETERS */
 const int screenWidth = 1280;
 const int screenHeight = 720;
-/* PLAYER PARAMETERS */
-int playerX = screenWidth / 2;
-int playerY = screenHeight / 2;
-double playerZ = 0.0;
-const int playerSize = 15;
-const int speed = 4;
 
-const double raysNumber = 128;
-const int FOV = M_PI / 3;
-const double halfFOV = FOV / 2;
-const double deltaAngle = FOV / raysNumber;
-const int maxDepth = 800;
-
+/* MAP PARAMETERS */
 const int MAP_WIDTH_CELLS = 16;
 const int MAP_HEIGHT_CELLS = 12;
 const int CELL_SIZE_X = 80;
 const int CELL_SIZE_Y = 60;
-double wallDistances[1280];
-const double PROJECTION_COEFF = screenWidth * 3;
+const int maxDepth = 800;
 
 int map[12][16] = {
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
@@ -40,6 +29,21 @@ int map[12][16] = {
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 };
 
+int playerX = CELL_SIZE_X * 1 + CELL_SIZE_X / 2;
+int playerY = CELL_SIZE_Y * 1 + CELL_SIZE_Y / 2;
+double playerZ = M_PI / 2;
+const int playerSize = 15;
+const int speed = 4;
+const double rotationSpeed = 0.05;
+
+const double raysNumber = 120;
+const double FOV = M_PI / 3;
+const double halfFOV = FOV / 2;
+const double deltaAngle = FOV / raysNumber;
+
+const double WALL_HEIGHT_SCALER = CELL_SIZE_Y;
+double wallDistances[120];
+
 void rayCasting() {
     double currentRayAngle = playerZ - halfFOV;
     
@@ -47,9 +51,8 @@ void rayCasting() {
         double sin_a = sin(currentRayAngle);
         double cos_a = cos(currentRayAngle);
         
-        double rayEndX = playerX;
-        double rayEndY = playerY;
         double currentDepth = 0.0;
+        
         for (int depth_step = 0; depth_step < maxDepth; depth_step++) {
             double x = playerX + depth_step * cos_a;
             double y = playerY + depth_step * sin_a;
@@ -62,113 +65,139 @@ void rayCasting() {
                 
                 if (map[mapY][mapX] == 1) {
                     currentDepth = depth_step;
-                    rayEndX = x;
-                    rayEndY = y;
                     break;
                 }
             } else {
                 currentDepth = depth_step;
-                rayEndX = x; 
-                rayEndY = y;
                 break;
             }
-
             if (depth_step == maxDepth - 1) {
                 currentDepth = maxDepth;
             }
-            rayEndX = x;
-            rayEndY = y;
         }
-
-        double correctedDepth = currentDepth * cos(currentRayAngle - playerZ);
         
-        wallDistances[ray] = correctedDepth; 
+        double correctedDepth = currentDepth * cos(currentRayAngle - playerZ);
+        wallDistances[ray] = fmax(0.1, correctedDepth);
 
-		/* DrawLine(playerX, playerY, (int)rayEndX, (int)rayEndY, DARKGRAY); */
         currentRayAngle += deltaAngle;
     }
 }
 
 void drawMap() {
-    for (int y = 0; y < 12; y++) {
-        for (int x = 0; x < 16; x++) {
+    const int MINI_MAP_SCALE = 4; 
+    const int MINI_MAP_CELL_X = CELL_SIZE_X / MINI_MAP_SCALE;
+    const int MINI_MAP_CELL_Y = CELL_SIZE_Y / MINI_MAP_SCALE;
+    const int MINI_MAP_OFFSET_X = 10;
+    const int MINI_MAP_OFFSET_Y = screenHeight - (MAP_HEIGHT_CELLS * MINI_MAP_CELL_Y) - 10;
+
+    for (int y = 0; y < MAP_HEIGHT_CELLS; y++) {
+        for (int x = 0; x < MAP_WIDTH_CELLS; x++) {
+            int drawX = MINI_MAP_OFFSET_X + x * MINI_MAP_CELL_X;
+            int drawY = MINI_MAP_OFFSET_Y + y * MINI_MAP_CELL_Y;
+
             switch (map[y][x]) {
                 case 0:
+                    DrawRectangle(drawX, drawY, MINI_MAP_CELL_X, MINI_MAP_CELL_Y, BLACK);
                     break;
                 case 1:
-                    DrawRectangleLines(x * 80, y * 60, 80, 60, RED);
+                    DrawRectangle(drawX, drawY, MINI_MAP_CELL_X, MINI_MAP_CELL_Y, GREEN);
                     break;
             }
+            DrawRectangleLines(drawX, drawY, MINI_MAP_CELL_X, MINI_MAP_CELL_Y, GRAY);
         }
     }
 }
 
 void player() {
-	if (IsKeyDown(KEY_W)) {
-		if (playerY >= playerSize) {
-			playerY = playerY - speed;
-		}
-	}
-		
-	if (IsKeyDown(KEY_S)) {
-		if (playerY <= screenHeight - playerSize) {
-			playerY = playerY + speed;
-		}
-	}
+  double move_dir_x = cos(playerZ);
+  double move_dir_y = sin(playerZ);
 
-	if (IsKeyDown(KEY_A)) {
-		if (playerX >= playerSize) {
-			playerX = playerX - speed;
-		}
-	}
-		
-	if (IsKeyDown(KEY_D)) {
-		if (playerX <= screenWidth - playerSize) {
-			playerX= playerX + speed;
-		}
-	}
+  int newPlayerX = playerX;
+  int newPlayerY = playerY;
 
-	if (IsKeyDown(KEY_LEFT)) {
-		playerZ = playerZ - 0.02;
-	}
-		
-	if (IsKeyDown(KEY_RIGHT)) {
-		playerZ = playerZ + 0.02;
-	}
-	
-	/* DrawCircle(playerX, playerY, playerSize, GREEN); */
+  if (IsKeyDown(KEY_W)) {
+    newPlayerX += move_dir_x * speed;
+    newPlayerY += move_dir_y * speed;
+  }
+  if (IsKeyDown(KEY_S)) {
+    newPlayerX -= move_dir_x * speed;
+    newPlayerY -= move_dir_y * speed;
+  }
+
+  if (IsKeyDown(KEY_A)) {
+    newPlayerX += cos(playerZ - M_PI/2) * speed;
+    newPlayerY += sin(playerZ - M_PI/2) * speed;
+  }
+  if (IsKeyDown(KEY_D)) {
+    newPlayerX += cos(playerZ + M_PI/2) * speed;
+    newPlayerY += sin(playerZ + M_PI/2) * speed;
+  }
+  
+  int mapCellX = (int)(newPlayerX / CELL_SIZE_X);
+  int mapCellY = (int)(newPlayerY / CELL_SIZE_Y);
+
+  if (mapCellX >= 0 && mapCellX < MAP_WIDTH_CELLS &&
+      mapCellY >= 0 && mapCellY < MAP_HEIGHT_CELLS) {
+      if (map[mapCellY][mapCellX] == 0) {
+          playerX = newPlayerX;
+          playerY = newPlayerY;
+      }
+  }
+
+  if (IsKeyDown(KEY_LEFT)) {
+    playerZ -= rotationSpeed;
+  }
+  if (IsKeyDown(KEY_RIGHT)) {
+    playerZ += rotationSpeed;
+  }
+  
+  const int MINI_MAP_SCALE = 4;
+  const int MINI_MAP_OFFSET_X = 10;
+  const int MINI_MAP_OFFSET_Y = screenHeight - (MAP_HEIGHT_CELLS * (CELL_SIZE_Y / MINI_MAP_SCALE)) - 10;
+
+  DrawCircle(MINI_MAP_OFFSET_X + (playerX / MINI_MAP_SCALE), MINI_MAP_OFFSET_Y + (playerY / MINI_MAP_SCALE), playerSize / MINI_MAP_SCALE, GREEN);
+  
+  DrawLine(MINI_MAP_OFFSET_X + (playerX / MINI_MAP_SCALE), MINI_MAP_OFFSET_Y + (playerY / MINI_MAP_SCALE),
+           MINI_MAP_OFFSET_X + (playerX / MINI_MAP_SCALE) + (int)(playerSize * 2 * move_dir_x / MINI_MAP_SCALE),
+           MINI_MAP_OFFSET_Y + (playerY / MINI_MAP_SCALE) + (int)(playerSize * 2 * move_dir_y / MINI_MAP_SCALE), BLUE);
 } 
 
-/*
 void drawFPS(int valueFPS) {
     char FPS[100];
     snprintf(FPS, sizeof(FPS), "FPS: %d", valueFPS);
-	DrawText(FPS, 10, 10, 32, GREEN);
+    DrawText(FPS, 10, 10, 20, WHITE);
 }
-*/
 
 int main() {
-    InitWindow(screenWidth, screenHeight, "");
+    InitWindow(screenWidth, screenHeight, "RayRender - A simple ray casting algorithm-based render written in C by nanoptushka");
+
     SetTargetFPS(60);
     while (!WindowShouldClose()) {
-    	char title[1000];
-    	snprintf(title, sizeof(title), "RayRender [fps: %d]", GetFPS());
-    	SetWindowTitle(title); 
+        char title[100];
+        snprintf(title, sizeof(title), "RayRender [fps: %d]", GetFPS());
+        SetWindowTitle(title); 
+        
         BeginDrawing();
             ClearBackground(DARKGREEN);
+            DrawRectangle(0, 0, screenWidth, screenHeight / 2, LIME);
+            player();
             rayCasting();
-            DrawRectangle(0, 0, 1280, 360, LIME);
             for (int ray = 0; ray < raysNumber; ray++) {
-                if (wallDistances[ray] > 0) {
-                    double wallHeight = screenWidth / wallDistances[ray];
-                    double screenX = ray * (screenWidth / raysNumber);
-                    DrawRectangle(screenX, (screenHeight / 2) - (wallHeight / 2), (screenWidth / raysNumber), wallHeight * 20, GREEN);
-                }
+            	const double PROJECTION_PLANE_DIST = (1280  / 2) / tan(halfFOV);
+                double wallHeight = (WALL_HEIGHT_SCALER * PROJECTION_PLANE_DIST / wallDistances[ray]);
+                
+                if (wallHeight > screenHeight) wallHeight = screenHeight;
+                if (wallHeight < 0) wallHeight = 0;
+
+                double screenX = ray * (screenWidth / raysNumber);
+                double columnWidth = (screenWidth / raysNumber);
+                
+                DrawRectangle(screenX, (screenHeight / 2) - (wallHeight / 2), columnWidth + 1, wallHeight, GREEN);
             }
-            /* drawMap(); */
-            player(playerX, playerY, speed);
-            /* drawFPS(GetFPS()); */
-        EndDrawing();
+            drawMap();
+            drawFPS(GetFPS());
+            
+        EndDrawing(); 
     }
     CloseWindow();
 
